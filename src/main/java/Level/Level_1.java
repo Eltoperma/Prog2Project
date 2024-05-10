cee4bb7b02b465537d274562db464b7222465d6d
Schulle8 <matthias.schulze@std.dhsh.de>
2024-05-10 11:35:37 +0200

    Neues Update

--- src/main/java/Level/Level_1.java
+++ src/main/java/Level/Level_1.java
@@ -25,60 +25,59 @@ public class Level_1 extends Level{
     private void configureTiles() {
 
         //user input
-        tiles.put(new Position(1, 1), new Tile(TileType.WALL));
-        tiles.put(new Position(1, 2), new Tile(TileType.STANDARD));
-        drawLineOfWalls(new Position(1, 1), new Position(1, 8));
+//        tiles.put(new Position(1, 1), new Tile(TileType.WALL));
+//        tiles.put(new Position(1, 2), new Tile(TileType.STANDARD));
+//        drawLineOfWalls(new Position(1, 1), new Position(1, 8));
+
+        //Start und Ziel tile festlegen
         //
+        drawLineOfWalls(new Position(1, 1), new Position(6, 1));
+
+        drawLineOfWalls(new Position(1, 1), new Position(1, 5));
+
+        tiles.put(new Position(2, 2), new Tile(TileType.STANDARD));
+
+        tiles.put(new Position(3, 2), new Tile(TileType.STANDARD));
+
+        tiles.put(new Position(4, 2), new Tile(TileType.STANDARD));
+
+        tiles.put(new Position(5, 2), new Tile(TileType.WALL));
+
+        tiles.put(new Position(6, 2), new Tile(TileType.WALL));
+
+        tiles.put(new Position(2, 3), new Tile(TileType.STANDARD));
+
+        tiles.put(new Position(3, 3), new Tile(TileType.STANDARD));
+
+        tiles.put(new Position(4, 3), new Tile(TileType.STANDARD));
+
+        tiles.put(new Position(5, 3), new Tile(TileType.STANDARD));
+
+        tiles.put(new Position(6, 3), new Tile(TileType.WALL));
+
+        tiles.put(new Position(2, 4), new Tile(TileType.STANDARD));
+
+        tiles.put(new Position(3, 4), new Tile(TileType.STANDARD));
+
+        tiles.put(new Position(4, 4), new Tile(TileType.STANDARD));
+
+        tiles.put(new Position(5, 4), new Tile(TileType.WALL));
+
+        tiles.put(new Position(6, 4), new Tile(TileType.WALL));
+
+        drawLineOfWalls(new Position(1, 5), new Position(3, 5));
+
+        tiles.put(new Position(4, 5), new Tile(TileType.STANDARD));
+
+        tiles.put(new Position(5, 5), new Tile(TileType.WALL));
+
+        drawLineOfWalls(new Position(3, 6), new Position(5, 6));
     }
 
    private void configureUpgrades(){
        //user input
         upgrades.put(new Position(1,2), Upgrades.ONE);
         //
-   }
-
-    private void configureRest(){
-        for(int x = 0; x < height; x++){
-            for(int y = 0; y < width; y++){
-                if(tiles.get(new Position(x, y)) == null){
-                    tiles.put(new Position(x, y), new Tile(TileType.NOTHING));
-                }
-            }
-        }
-    }
-
-public class Level_1 extends Level{
-
-    @Override
-    public void configure(){
-        height = 10;
-        width = 10;
-
-        configureTiles();
-        configureUpgrades();
-        configureRest();
-    }
-
-    private void configureTiles() {
-        tiles.put(new Position(1, 1), new Tile(TileType.WALL));
-
-        tiles.put(new Position(2, 1), new Tile(TileType.WALL));
-
-        tiles.put(new Position(3, 1), new Tile(TileType.WALL));
-
-        tiles.put(new Position(4, 1), new Tile(TileType.WALL));
-
-        tiles.put(new Position(5, 1), new Tile(TileType.WALL));
-
-        tiles.put(new Position(6, 1), new Tile(TileType.WALL));
-
-        tiles.put(new Position(1, 2), new Tile(TileType.WALL));
-
-        tiles.put(new Position(1, 1), new Tile(TileType.WALL));
-    }
-
-   private void configureUpgrades(){
-        upgrades.put(new Position(1,2), Upgrades.ONE);
    }
 
     private void configureRest(){
